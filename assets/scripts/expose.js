c0f3ebf867e6ed90c80456b84276ba4f2b68f0fb
XtremePratham <prathamfastsavla@gmail.com>
2025-05-06 15:03:52 -0700

    finished expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,46 @@
-// expose.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const hornSelect = document.getElementById('horn-select');
+  const hornImage = document.querySelector('img[alt="No image selected"]');
+  const audio = document.querySelector('audio');
+  const volumeInput = document.getElementById('volume');
+  const volumeIcon = document.querySelector('#volume-controls img');
+  const playButton = document.querySelector('button');
+  const jsConfetti = new JSConfetti();
+
+  hornSelect.addEventListener('change', () => {
+    const selectedHorn = hornSelect.value;
+    hornImage.src = `assets/images/${selectedHorn}.svg`;
+    hornImage.alt = selectedHorn.replace('-', ' ');
+    audio.src = `assets/audio/${selectedHorn}.mp3`;
+  });
+
+  volumeInput.addEventListener('input', () => {
+    const volume = volumeInput.value;
+    audio.volume = volume / 100;
+
+    // Update volume icon
+    let volumeLevel;
+    if (volume == 0) {
+      volumeLevel = 0;
+    } else if (volume < 33) {
+      volumeLevel = 1;
+    } else if (volume < 67) {
+      volumeLevel = 2;
+    } else {
+      volumeLevel = 3;
+    }
+    volumeIcon.src = `assets/icons/volume-level-${volumeLevel}.svg`;
+    volumeIcon.alt = `Volume level ${volumeLevel}`;
+  });
+
+  playButton.addEventListener('click', () => {
+    if (audio.src) {
+      audio.play();
+      if (hornSelect.value === 'party-horn') {
+        jsConfetti.addConfetti();
+      }
+    }
+  });
 }
